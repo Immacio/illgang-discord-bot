8b1799d362549fc40144e97426afb775c78b8171
Maurice <maurice@app-bar.com>
2022-08-17 16:21:15 -0400

    feat: Initial Commands and init

---
+++ index.ts
@@ -0,0 +1,32 @@
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (commandName === "server") {
+    await interaction.reply(
+      `Server name: ${interaction.guild.name}\nMember Count: ${interaction.guild.memberCount}`
+    );
+  } else if (commandName === "user") {
+    await interaction.reply(`Your name: ${interaction.user.tag}`);
+  } else if (commandName === "help") {
+    await interaction.reply(`Available commands: ping, server, user, help`);
+  }
+});
+
+// Login to Discord with your client's token
+client.login(process.env.DISCORD_AUTH_TOKEN);
